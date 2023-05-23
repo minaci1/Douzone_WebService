433f21bc73f76501854bc3823df4014440ad0d78
minaci1 <minaci_@naver.com>
2023-05-23 23:11:37 +0900

    fix:  HMTL5 프로젝트 수정

--- GroupQuiz/script/memo.js
+++ GroupQuiz/script/memo.js
@@ -1,25 +1,3 @@
-/*
-HTML 드래그 앤 드랍 API < MDN에 detail
-https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
-
-Drag Event Properties
-이벤트		이벤트 핸들러	설명
->drag		ondrag		요소나 텍스트 블록을 드래그 할 때 발생한다.
-dragend		ondragend	드래그를 끝냈을 때 발생한다. (마우스 버튼을 떼거나 ESC 키를 누를 때) (드래그 끝내기를 보시오)
-dragenter	ondragenter	드래그한 요소나 텍스트 블록을 적합한 드롭 대상위에 올라갔을 때 발생한다. (드롭 대상 지정하기를 보시오.)
-dragexit	ondragexit	요소가 더 이상 드래그의 직접적인 대상이 아닐 때 발생한다.
-dragleave	ondragleave	드래그하는 요소나 텍스트 블록이 적합한 드롭 대상에서 벗어났을 때 발생한다.
->dragover	ondragover	요소나 텍스트 블록을 적합한 드롭 대상 위로 지나갈 때 발생한다. (매 수백 밀리초마다 발생한다.)
-dragstart	ondragstart	사용자가 요소나 텍스트 블록을 드래그하기 시작했을 때 발생한다. (드래그 시작하기를 보시오.)
-**drop		ondrop		요소나 텍스트 블록을 적합한 드롭 대상에 드롭했을 때 발생한다. (드롭하기를 보시오.)
-
-드래그 시작 : 
-dragstart > drag[계속] > drop가능한 요소에 올라왔다면 > dragenter > "dragover[계속]" > drop요소에 drop시 "drop"
-					  > drop가능한 요소에 있다가 벗어나면서 dragleave
-
-참고: dragstart와 dragend 이벤트는 파일을 브라우저로 드래그할 때는 발생하지 않습니다.
-*/
-
 const datas = JSON.parse(localStorage.getItem("places")); //로컬스토리지에 있는 object를 JSON으로 파싱해오고 datas로 전달
 let item = "";
 if (datas != null) {
@@ -27,18 +5,49 @@ if (datas != null) {
   //document.getElementById("content-box").innerText = datas;
   for (let i = 0; i < datas.length; i++) {
     //로컬스토리지 길이만큼 받아오기
+    let id = datas[i].id;
+    let color = datas[i].color;
     let name = datas[i].name;
     let addr = datas[i].addr;
     let lat = datas[i].lat;
     let lng = datas[i].lng;
 
-    console.log(name);
-    console.log(addr);
-    console.log(lat);
-    console.log(lng);
+    let contents = "";
 
-    item += `
-    <div class="content-box" draggable="true"> 
+    console.log(color + " " + id);
+    switch (color) {
+      case "gray":
+        item = getAdd("gray", id, name, addr, lat, lng);
+        console.log(item);
+        contents = document.getElementsByClassName("dropzone")[0];
+        contents.innerHTML += item;
+        break;
+      case "green":
+        item = getAdd("green", id, name, addr, lat, lng);
+
+        contents = document.getElementsByClassName("dropzone")[1];
+        contents.innerHTML += item;
+        break;
+      case "blue":
+        item = getAdd("blue", id, name, addr, lat, lng);
+
+        contents = document.getElementsByClassName("dropzone")[2];
+        contents.innerHTML += item;
+        break;
+      case "yellow":
+        item = getAdd("yellow", id, name, addr, lat, lng);
+
+        contents = document.getElementsByClassName("dropzone")[3];
+        contents.innerHTML += item;
+        break;
+      default:
+        console.log("잘못입력");
+    }
+  }
+}
+
+function getAdd(color, id, name, addr, lat, lng) {
+  return (item = `<div class="content-box ${color} id${id}" draggable="true"> 
       <div class="name">
         <p class="place_name">${name}</p>
       </div>
@@ -51,25 +60,14 @@ if (datas != null) {
       <div class="lng">
         <p class="lng">${Number(lng).toFixed(2)}</p>
       </div>
-    </div>`;
-  }
-  //let contents = document.getElementById("getpostit");
-
-  let contents = document.getElementsByClassName("dropzone")[0];
-  contents.innerHTML += item;
+    </div>`);
 }
-
 //드래그앤드롭 시작
 
 window.onload = function () {
   var cards = document.querySelectorAll(".content-box"); //.card를 가진 elements 배열
   var dropzones = document.querySelectorAll(".dropzone"); //.dropzone을 가진 elements 배열
-  // cards.forEach(function(card){})
-  // 와 같은 함수를 Arrow Function으로..
 
-  // https://yuddomack.tistory.com/entry/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8
-  // forEach문 -> JS Ex43_..
-  // drag할 card들 event attach
   cards.forEach((card) => {
     card.addEventListener("dragstart", dragStart);
     card.addEventListener("drag", drag); // drag 여러번 발생
@@ -77,7 +75,6 @@ window.onload = function () {
   });
 
   /*---------- .card : Drag 요소 함수들 ----------*/
-
   function dragStart() {
     //console.log("카드 : dragstart" );
     dropzones.forEach((dropzone) => dropzone.classList.add("highlight"));
@@ -94,9 +91,27 @@ window.onload = function () {
   function dragEnd() {
     console.log("카드 : drag end");
     dropzones.forEach((dropzone) => dropzone.classList.remove("highlight"));
-
-    // this = card
     this.classList.remove("is-dragging");
+    let newColor = this.parentNode.classList[1];
+    let oldColor = this.classList[1];
+    console.log("새로운색:" + newColor);
+    console.log("원래색:" + oldColor);
+    this.classList.replace(oldColor, newColor);
+
+    let totaldata = JSON.parse(localStorage.getItem("places"));
+    //console.log(totaldata);
+    console.log(this.classList);
+    console.log("번호 : " + this.classList[2].substring(2)); //id0 중 0출력
+    let id = this.classList[2].substring(2); //id의 값
+
+    for (let index in totaldata) {
+      if (totaldata[index].id == id) {
+        //스토리지의 값
+        totaldata[index].color = newColor;
+
+        localStorage.setItem("places", JSON.stringify(totaldata));
+      }
+    }
   }
 
   dropzones.forEach((dropzone) => {
@@ -113,7 +128,8 @@ window.onload = function () {
     //this = dropzone
   }
 
-  function dragOver() {
+  function dragOver(event) {
+    event.preventDefault(); // 이 부분을 추가해야 drop 이벤트가 발생합니다.
     //console.log("드랍 : dragOver");
     //this = dropzone
     //console.log(this);
